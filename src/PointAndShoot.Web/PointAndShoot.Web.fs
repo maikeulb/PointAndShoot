004d39b6f3b4a4ba70154cb7d19e295da524facb
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-01-26 23:15:27 -0500

    Add home page route

--- src/PointAndShoot.Web/PointAndShoot.Web.fs
+++ src/PointAndShoot.Web/PointAndShoot.Web.fs
@@ -1,9 +1,41 @@
-module PointAndShootWeb.Main
+module PointAndShoot.Main
 
 open Suave
 open Suave.Successful
+open Suave.Filters
+open Suave.DotLiquid
+open Suave.Files
+open Suave.Operators
+open Suave.RequestErrors
+open System.Reflection
+open System.IO
+open System
+
+let currentPath =
+  Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+
+let initDotLiquid () =
+  setCSharpNamingConvention ()
+  let templatesDir = Path.Combine(currentPath, "views")
+  setTemplatesDir templatesDir
+
+let serveStatic=
+  let faviconPath = 
+    Path.Combine(currentPath, "static", "images", "favicon.ico")
+  choose [
+    pathRegex "/static/*" >=> browseHome
+    path "/favicon.ico" >=> file faviconPath
+  ]
 
 [<EntryPoint>]
 let main argv =
-    startWebServer defaultConfig (OK "Hello World!")
-    0
+  initDotLiquid ()
+
+  let app = 
+    choose [
+      serveStatic
+      path "/" >=> page "/home.liquid" ""
+  ]
+
+  startWebServer defaultConfig app
+  0
